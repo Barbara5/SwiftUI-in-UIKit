be376df5344fa4147a5d54250b0d81cf3d2d9e73
Barbara <barbarabeatrice95@gmail.com>
2022-03-26 17:37:56 +0100

    changed animation and added navigation controller

--- SwiftUI in UIKit/SwiftUI in UIKit/LoadingAnimationView.swift
+++ SwiftUI in UIKit/SwiftUI in UIKit/LoadingAnimationView.swift
@@ -9,28 +9,34 @@ import SwiftUI
 
 struct LoadingAnimationView: View {
     
-    @State var scale : CGFloat = 1
-    
     var body: some View {
         
-        HStack (spacing: 30){
-            ForEach (0...2, id: \.self) { item in
-                RoundedRectangle(cornerRadius: 6)
-                    .foregroundColor(.teal)
-                    .frame(width: 20, height: 20)
-                    .opacity(0.6)
-                    .scaleEffect(scale)
-                
-                    .onAppear{
-                        let animation = Animation.spring()
-                        let repeatAnimation = animation.repeatForever(autoreverses: true)
-                        
-                        withAnimation(repeatAnimation) {
-                            scale = 1.6
-                        }
-                    }
-            }
+        HStack (spacing: 30) {
+            RectangleView()
+            RectangleView(delayAnimation: 0.4)
+            RectangleView(delayAnimation: 0.6)
         }
+    }
+}
+
+
+struct RectangleView : View {
+    
+    @State var delayAnimation : Double = 0
+    @State var originalOffset: CGFloat = 0
+    @State var animationOffset: CGFloat = -15
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 8)
+            .foregroundColor(.teal)
+            .frame(width: 40, height: 40)
+            .offset(y: originalOffset)
+            .onAppear {
+                withAnimation(Animation.linear(duration: 0.7).repeatForever(autoreverses: true).delay(delayAnimation)) {
+                    swap(&self.originalOffset, &self.animationOffset)
+                }
+            }
+        
     }
 }
 
